bdcb56bd22956658bef7f213a2e20a6ce099424d
AbraaoAlves <abraao.teodosio@gmail.com>
2023-09-16 19:05:57 +0000

    fix(deploy): config export build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images:{unoptimized: true}
+};
+
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  
 })
 
-module.exports = withNextra()
+module.exports = withNextra(nextConfig);
